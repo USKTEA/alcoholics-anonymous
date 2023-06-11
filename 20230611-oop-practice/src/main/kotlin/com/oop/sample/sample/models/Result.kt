ca86eeaf84512595dba8fef78eef80f6f126b2cb
usktea <tjrxo1234@gmail.com>
2023-06-11 23:46:54 +0900

    oop수정함
    bean은 타입을 먼저 탐색하고 동일 type이 있을 때 이름으로 의존성 주입한다

--- 20230611-oop-practice/src/main/kotlin/com/oop/sample/sample/models/Result.kt
+++ 20230611-oop-practice/src/main/kotlin/com/oop/sample/sample/models/Result.kt
@@ -3,11 +3,15 @@ package com.oop.sample.sample.models
 import com.oop.sample.sample.dtos.ResultDto
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
+@Table(name = "calculate_result")
 class Result(
-    private val value: Int
+    private val calculatedValue: Int
 ) {
+    @Id
     @GeneratedValue
     private var id: Long? = null
     private var active = true
@@ -17,6 +21,6 @@ class Result(
     }
 
     fun toDto(): ResultDto {
-        return ResultDto(id!!, value)
+        return ResultDto(id!!, calculatedValue)
     }
 }
--- 20230611-oop-practice/src/main/kotlin/com/oop/sample/sample/utils/Calculable.kt
+++ 20230611-oop-practice/src/main/kotlin/com/oop/sample/sample/utils/Calculable.kt
@@ -1,6 +1,7 @@
 package com.oop.sample.sample.utils
 
 interface Calculable {
+    fun calculate(operator: String, firstOperand: Int, secondOperand: Int): Int
     fun add(firstOperand: Int, secondOperand: Int): Int
     fun minus(firstOperand: Int, secondOperand: Int): Int
     fun multiply(firstOperand: Int, secondOperand: Int): Int
