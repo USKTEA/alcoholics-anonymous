dc2ec97f651d3d1b43b24de8a81ff80f0a34ff40
Rocky <rocky@amond.io>
2023-07-05 18:11:50 +0900

    엑셀 100만건 다운로드 가능하게 수정
    SXSSF를 사용해 100만건 다운 받을 수 있도록 수정함F

--- 20230702-poi-practice/server/src/main/kotlin/com/usktea/demo/controller/PaymentController.kt
+++ 20230702-poi-practice/server/src/main/kotlin/com/usktea/demo/controller/PaymentController.kt
@@ -4,6 +4,7 @@ import com.usktea.demo.dtos.PaymentsDto
 import com.usktea.demo.infra.ExcelFileResponseGenerator
 import com.usktea.demo.services.PaymentService
 import org.apache.poi.ss.usermodel.Workbook
+import org.apache.poi.xssf.streaming.SXSSFWorkbook
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import org.springframework.core.io.FileSystemResource
 import org.springframework.data.domain.PageRequest
@@ -24,19 +25,120 @@ class PaymentController(
     private val paymentService: PaymentService
 ) {
 
+//    @GetMapping("excel")
+//    fun getPaymentExcel(): ResponseEntity<FileSystemResource> {
+//        val sort = Sort.by(Sort.Direction.DESC, "id")
+//        val size = 5000
+//        var page = PageRequest.of(0, size, sort)
+//        val createDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+//
+//        val file = File.createTempFile("Payments_${createDate}", ".xlsx")
+//        file.deleteOnExit()
+//
+//        val workbook: Workbook = XSSFWorkbook()
+//        val sheet = workbook.createSheet("Payment_$createDate")
+//        val headerFont = workbook.createFont()
+//        headerFont.bold = true
+//        headerFont.fontHeightInPoints = 14
+//
+//        val headerCellStyle = workbook.createCellStyle()
+//        headerCellStyle.setFont(headerFont)
+//        val headerRow = sheet.createRow(0)
+//        val columns = arrayOf(
+//            "Price",
+//            "Amount",
+//            "Product_Name"
+//        )
+//
+//        for (i in columns.indices) {
+//            val cell = headerRow.createCell(i)
+//            cell.setCellValue(columns[i])
+//            cell.cellStyle = headerCellStyle
+//        }
+//
+//        val totalCount = paymentService.countAllPayments()
+//        var rowNumber = 1
+//
+//        for (i: Long in 0..totalCount / size) {
+//            println(page.pageNumber)
+//            val payments = paymentService.getAllPayments(page)
+//            for (payment in payments) {
+//                val row = sheet.createRow(rowNumber)
+//                row.createCell(0).setCellValue(payment.price.toDouble())
+//                row.createCell(1).setCellValue(payment.amount.toDouble())
+//                row.createCell(2).setCellValue(payment.productName)
+//                rowNumber++
+//            }
+//
+//            val fileOut = FileOutputStream(file)
+//            workbook.write(fileOut)
+//            fileOut.close()
+//            page = page.next()
+//        }
+//
+//        workbook.close()
+//        return ExcelFileResponseGenerator.generate(file)
+//    }
+
+//    @GetMapping("excel")
+//    fun getPaymentExcel2(): ResponseEntity<FileSystemResource> {
+//        val createDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+//
+//        val file = File.createTempFile("Payments_${createDate}", ".xlsx")
+//        file.deleteOnExit()
+//
+//        val workbook: Workbook = XSSFWorkbook()
+//        val sheet = workbook.createSheet("Payment_$createDate")
+//        val headerFont = workbook.createFont()
+//        headerFont.bold = true
+//        headerFont.fontHeightInPoints = 14
+//
+//        val headerCellStyle = workbook.createCellStyle()
+//        headerCellStyle.setFont(headerFont)
+//        val headerRow = sheet.createRow(0)
+//        val columns = arrayOf(
+//            "Price",
+//            "Amount",
+//            "Product_Name"
+//        )
+//
+//        for (i in columns.indices) {
+//            val cell = headerRow.createCell(i)
+//            cell.setCellValue(columns[i])
+//            cell.cellStyle = headerCellStyle
+//        }
+//
+//        var rowNumber = 1
+//        val payments = paymentService.getAllPayments()
+//
+//        for (payment in payments) {
+//            val row = sheet.createRow(rowNumber)
+//            row.createCell(0).setCellValue(payment.price.toDouble())
+//            row.createCell(1).setCellValue(payment.amount.toDouble())
+//            row.createCell(2).setCellValue(payment.productName)
+//            rowNumber++
+//        }
+//
+//        val fileOut = FileOutputStream(file)
+//        workbook.write(fileOut)
+//        fileOut.close()
+//
+//        workbook.close()
+//        return ExcelFileResponseGenerator.generate(file)
+//    }
+
     @GetMapping("excel")
-     fun getPaymentExcel(): ResponseEntity<FileSystemResource> {
+    fun getPaymentExcel(): ResponseEntity<FileSystemResource> {
         val sort = Sort.by(Sort.Direction.DESC, "id")
         val size = 5000
         var page = PageRequest.of(0, size, sort)
         val createDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))
 
         val file = File.createTempFile("Payments_${createDate}", ".xlsx")
-        file.deleteOnExit()
-
-        val workbook: Workbook = XSSFWorkbook()
+        val workbook: Workbook = SXSSFWorkbook(100000)
         val sheet = workbook.createSheet("Payment_$createDate")
         val headerFont = workbook.createFont()
+
         headerFont.bold = true
         headerFont.fontHeightInPoints = 14
 
@@ -69,13 +171,15 @@ class PaymentController(
                 rowNumber++
             }
 
-            val fileOut = FileOutputStream(file)
-            workbook.write(fileOut)
-            fileOut.close()
             page = page.next()
         }
 
+        val fileOut = FileOutputStream(file)
+        file.deleteOnExit()
+        workbook.write(fileOut)
         workbook.close()
+        fileOut.close()
+
         return ExcelFileResponseGenerator.generate(file)
     }
 
